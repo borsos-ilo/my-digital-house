bdbc6e6ce1bab22ce393de1270140e91b9197bbf
Ilona Kedracka <ilona.kedracka@gmail.com>
2024-05-02 18:12:16 +0200

    non-jumping ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,10 +14,13 @@ export default function Home() {
   console.log("in index: " + postCategory)
   return (
     <main className={`flex min-h-screen justify-around flex-col mx-28 my-10`}>
-      <h1 className={`font-pd text-6xl`}>Mój cyfrowy dom</h1>
-      <p className="border-l-2 border-cyan-500 p-2 font-pd italic text-lg">Nie mogłam żyć w żadnym ze światów, które były mi oferowane - w świecie moich rodziców, w świecie wojny, w świecie polityki. Musiałam stworzyć sobie swój własny świat - klimat, kraj, atmosferę, w którym mogę oddychać, rządzić, stwarzać się ponownie, kiedy zniszczyło mnie życie. Wierzę, że to jest powodem stojącym za każdym dziełem sztuki. - <a className="text-cyan-500 not-italic hover:underline decoration-cyan-500 after:content-['_↗'] ..." href="https://pl.wikipedia.org/wiki/Ana%C3%AFs_Nin" target="_blank">Anaïs Nin</a></p>
+      <h1 className="font-pd flex-1 text-6xl">Mój cyfrowy dom</h1>
+      <p className="border-l-2 flex-1 border-cyan-500 p-2 font-pd italic text-lg">Nie mogłam żyć w żadnym ze światów, które były mi oferowane - w świecie moich rodziców, w świecie wojny, w świecie polityki. Musiałam stworzyć sobie swój własny świat - klimat, kraj, atmosferę, w którym mogę oddychać, rządzić, stwarzać się ponownie, kiedy zniszczyło mnie życie. Wierzę, że to jest powodem stojącym za każdym dziełem sztuki. - <a className="text-cyan-500 not-italic hover:underline decoration-cyan-500 after:content-['_↗'] ..." href="https://pl.wikipedia.org/wiki/Ana%C3%AFs_Nin" target="_blank">Anaïs Nin</a></p>
       <NavBar onCategorySelect={handlePostCategory} selectedCategory={postCategory}/>
-      <PostsList category={postCategory}/>
+      <div className={"min-h-[500px]"}>
+        <PostsList category={postCategory}/>
+      </div>
+      
       </main>
     
   );

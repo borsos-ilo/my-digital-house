5e49e45c4888405350319f34255f1602ae83b532
Ilona Kedracka <ilona.kedracka@gmail.com>
2024-05-03 21:32:59 +0200

    added nav

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,13 +1,18 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import sunflower from "/public/sunflower.png"
 
 type NoProps={}
 
 const TopNav: React.FC<NoProps> = () => {
     return (
-        <div className='mb-10'>
-            <Link href={'/'} className='font-pd'>
-                Mój cyfrowy dom
+        <div className='mb-10 grid grid-cols-2 max-h-8 border'>
+            <Link href={"/"}>
+                <Image src={sunflower} alt="sunflower icon" width={30} height={30}/>
+            </Link>
+            <Link href={'/o-mnie'} className='font-pd'>
+                O mnie
             </Link>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import NavBar from "@/components/NavBar";
 import  PostsList  from "@/components/PostsList";
 import { useState } from "react";
-import MainTopNav from "@/components/MainTopNav";
+import TopNav from "@/components/TopNav";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +13,6 @@ export default function Home() {
   console.log("in index: " + postCategory)
   return (
     <main className={`flex min-h-screen justify-around flex-col mx-28 my-10`}>
-      <MainTopNav/>
       <h1 className="font-pd flex-1 text-6xl py-5">Mój cyfrowy dom</h1>
       <p className="border-l-2 flex-1 rounded-sm border-cyan-500 pl-5 font-pd italic text-lg">Nie mogłam żyć w żadnym ze światów, które były mi oferowane - w świecie moich rodziców, w świecie wojny, w świecie polityki. Musiałam stworzyć sobie swój własny świat - klimat, kraj, atmosferę, w którym mogę oddychać, rządzić, stwarzać się ponownie, kiedy zniszczyło mnie życie. Wierzę, że to jest powodem stojącym za każdym dziełem sztuki. - <a className="text-cyan-500 not-italic hover:underline decoration-cyan-500 after:content-['_↗'] ..." href="https://pl.wikipedia.org/wiki/Ana%C3%AFs_Nin" target="_blank">Anaïs Nin</a></p>
       <NavBar onCategorySelect={handlePostCategory} selectedCategory={postCategory}/>

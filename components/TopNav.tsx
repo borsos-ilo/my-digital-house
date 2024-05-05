bdc1f02b0e380c1ddcf15b1c10e9792edac793fd
Ilona Kedracka <ilona.kedracka@gmail.com>
2024-05-05 09:08:17 +0200

    remove motion from page title

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import sunflower from "/public/sunflower.png"
+import { motion } from "framer-motion"
 
 type NoProps={}
 
@@ -11,9 +12,9 @@ const TopNav: React.FC<NoProps> = () => {
             <Link href={"/"} className='justify-self-start p-5 ml-5'>
                 <Image className='transition ease-in-out delay-50  hover:scale-125 duration-300' src={sunflower} alt="sunflower icon" width={50} height={50}/>
             </Link>
-            <Link href={'/o-mnie'} className='p-5 mr-5 justify-self-end self-center font-hs text-2xl font-semibold hover:underline decoration-cyan-300'>
-                O mnie
-            </Link>
+                <Link href={'/o-mnie'} className='p-5 mr-5 justify-self-end self-center font-hs text-2xl font-semibold hover:underline decoration-cyan-300'>
+                    O mnie
+                </Link>
         </div>
     )
 }    
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import NavBar from "@/components/NavBar";
 import  PostsList  from "@/components/PostsList";
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const controls = useAnimation();
   const [postCategory, setPostCategory]=useState<string | null>(null)
 
   // Select category on click, deselect on another click
@@ -12,8 +13,11 @@ export default function Home() {
   
   return (
     <main className={`flex min-h-screen flex-col mx-28 my-5`}>
-      <motion.h1 whileHover={{ y: -10, transition: { type: 'spring', stiffness: 300, damping: 10 } }} className="font-arapey flex-1 text-6xl py-5">Mój cyfrowy ogród</motion.h1>
-      <p className="border-l-2 flex-1 rounded-sm border-cyan-500 pl-5 font-hs italic text-lg">Nie mogłam żyć w żadnym ze światów, które były mi oferowane - w świecie moich rodziców, w świecie wojny, w świecie polityki. Musiałam stworzyć sobie swój własny świat - klimat, kraj, atmosferę, w którym mogę oddychać, rządzić, stwarzać się ponownie, kiedy zniszczyło mnie życie. Wierzę, że to jest powodem stojącym za każdym dziełem sztuki. - <a className="text-cyan-500 not-italic hover:underline decoration-cyan-500" href="https://pl.wikipedia.org/wiki/Ana%C3%AFs_Nin" target="_blank">Anaïs Nin</a></p>
+      <h1 className="font-arapey flex-1 text-6xl py-5">
+        Mój cyfrowy ogród</h1>
+      <p className="border-l-2 flex-1 rounded-sm border-cyan-500 pl-5 font-hs italic text-lg">
+        Nie mogłam żyć w żadnym ze światów, które były mi oferowane - w świecie moich rodziców, w świecie wojny, w świecie polityki. Musiałam stworzyć sobie swój własny świat - klimat, kraj, atmosferę, w którym mogę oddychać, rządzić, stwarzać się ponownie, kiedy zniszczyło mnie życie. Wierzę, że to jest powodem stojącym za każdym dziełem sztuki. - <a className="text-cyan-500 not-italic hover:underline decoration-cyan-500" href="https://pl.wikipedia.org/wiki/Ana%C3%AFs_Nin" target="_blank">Anaïs Nin</a>
+      </p>
       <NavBar onCategorySelect={handlePostCategory} selectedCategory={postCategory}/>
       <div className={"min-h-[500px]"}>
         <PostsList category={postCategory}/>

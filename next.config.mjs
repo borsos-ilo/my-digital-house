21ff0e010a3b24de55c0c87ab5fdef06953eda50
Ilona Kedracka <ilona.kedracka@gmail.com>
2024-05-06 22:02:06 +0200

    just basic options for i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["pl", "en"],
+    defaultLocale: "pl",
+  },
 };
 
 export default nextConfig;
